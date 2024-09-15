5adc3229bf8a96ded046549ae23f94222940b49b
Shintaro Takemoto <kvrg68@gmail.com>
2024-09-15 13:41:04 +0900

    video until 3

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,62 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <v-app>
+    <v-navigation-drawer v-model="drawer">
+      <v-sheet
+        class="pa-4"
+        color="grey-lighten-4"
+      >
+        <v-avatar
+          class="mb-4"
+          color="grey-darken-1"
+          size="64"
+        ></v-avatar>
+
+        <div>john@google.com</div>
+      </v-sheet>
+
+      <v-divider></v-divider>
+
+      <v-list>
+        <v-list-item
+          v-for="[icon, text, to] in links"
+          :key="icon"
+          :prepend-icon="icon"
+          :title="text"
+          :to="to"
+          link
+        ></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <v-container
+          class="py-8 px-6"
+          fluid
+        >
+        <router-view/>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
+
+
+<script setup>
+  import { ref } from 'vue'
+  import '@mdi/font/css/materialdesignicons.css'
+
+  const links = [
+    ['mdi-inbox-arrow-down', 'Inbox', '/'],
+    ['mdi-send', 'Send', '/about'],
+    ['mdi-delete', 'Trash', '/about'],
+    ['mdi-alert-octagon', 'Spam', '/about'],
+  ]
+
+  const drawer = ref(null)
+</script>
+
+
 
 <style>
 #app {
@@ -26,5 +80,13 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+nav.v-navigation-drawer.v-navigation-drawer--left.v-navigation-drawer--active.v-theme--light {
+    background-color: #89cfcf;
+}
+
+.bg-grey-lighten-4 {
+    background-color: #f5f5f500 !important;
 }
 </style>
