635fcdff12592205f95594d1045ae69440397bdd
Shintaro Takemoto <kvrg68@gmail.com>
2024-09-15 11:55:37 +0900

    second push

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+import App from './App.vue'
+import router from './router'
+
+//createApp(App).mount('#app')
+
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
+
+createApp(App).use(router).use(vuetify).mount('#app')
